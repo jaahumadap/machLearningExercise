03717417a21db1abc207b0d0e50a72d07d1d01bc
Jorge <jahumada@conservation.org>
2016-02-25 11:01:15 -0500

    Latest changes

--- exploratory analysis.R
+++ exploratory analysis.R
@@ -3,6 +3,7 @@
 train <- read.csv("pml-training.csv",h=T)
 test <- read.csv("pml-testing.csv",h=T)
 dim(train); dim(test)
+library(plyr)
 library(dplyr)
 library(parallel)
 library(doParallel)
@@ -18,7 +19,7 @@ valindx <- createDataPartition(train$classe,p=0.80)[[1]]
 train <- train[valindx,]
 val <- train[-valindx,]
 indx <- grep("kurtosis_|skewness_|max_|min_|amplitude_|var_|avg_|stddev_|timestamp",names(test))
-test <- test[,-indx]
+test <- test[,-c(1,indx)]
 #tests
 
 library(parallel)
@@ -32,14 +33,40 @@ fitControl <- trainControl(method = "cv",
                            number = 15,
                            allowParallel = TRUE)
 
-x <- train[,-56]
+x <- train[,-c(2,3,56)]
 y <- train[,56]
 
-system.time(modrf <- train(x,y,data=train,method="rf",trainControl = fitControl))
+modrf <- train(x,y,data=train,method="rf",trainControl = fitControl)
 
 modrfPCA <- train(x,y,data=train,method="rf", preProcess="pca", trainControl = fitControl)
 
 
 stopCluster(cluster)
-modgbm <- train(x,y,data=train,method="gbm",verbose=FALSE) #did not work
-modbboost <- train(x,y,data=train,method="blackboost")
+modgbm2 <- train(classe ~ ., data=train[,-c(2,3)], method="gbm",verbose=FALSE) #did not work
+modgbmPCA <- train(classe ~ ., data=train[,-c(2,3)], method="gbm",verbose=FALSE, preProcess = "pca")
+
+# Display the results
+results <- resamples(list(RF=modrf, RF_PCA=modrfPCA, GBM = modgbm, GBM_PCA = modgbmPCA))
+
+bwplot(results)
+
+#Predict classe in validation set and calculate accuracy
+#Random forest
+predrf <- predict(modrf, val)
+confusionMatrix(predrf,val$classe)$overall
+
+#Random forests with PCA
+predrfPCA <- predict(modrfPCA, val)
+confusionMatrix(predrfPCA,val$classe)$overall
+
+#GBM
+predgbm <- predict(modgbm, val)
+confusionMatrix(predgbm,val$classe)
+
+#GBM with PCA
+predgbmPCA <- predict(modgbmPCA, val)
+confusionMatrix(predgbmPCA,val$classe)
+
+#generate new predictions for test data set
+preds <- predict(modrf, test[,-56])
+data.frame(problem_id=test[,56], prediction=preds)
